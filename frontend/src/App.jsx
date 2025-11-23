f9429b4fe48f1ba24af67a9c64fdd47c00928e4a
Gary <117497221+GaryZhous@users.noreply.github.com>
2025-11-23 12:34:17 -0500

    updated path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,27 +5,32 @@ import Dashboard from './pages/Dashboard.jsx';
 import Events from './pages/Events.jsx';
 import Promotions from './pages/Promotions.jsx';
 import EventDetails from './pages/EventDetails.jsx';
-import ProfileSettings from './pages/ProfileSettings.jsx'
+import ProfileSettings from './pages/ProfileSettings.jsx';
+import Login from './pages/Login.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
     <>
       <Toaster position="top-right" richColors />
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route path="events" element={<Events />} />
-            <Route path="events/:id" element={<EventDetails />} />
-            <Route path="settings/profile" element={<Profile />}>
-              <Route path="edit" element={<ProfileSettings />} />
+        <AuthProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/" element={<Layout />}>
+              <Route path="events" element={<Events />} />
+              <Route path="events/:id" element={<EventDetails />} />
+              <Route path="settings/profile" element={<Profile />}>
+                <Route path="edit" element={<ProfileSettings />} />
+              </Route>
+              <Route path="promotions" element={<Promotions />} />
+              <Route path="/" element={<Dashboard />} />
+              {/* add other route paths as they are created*/}
             </Route>
-            <Route path="promotions" element={<Promotions />} />
-            <Route path="/" element={<Dashboard />} />
-            {/* add other route paths as they are created*/}
-          </Route>
-        </Routes>
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </>
   );

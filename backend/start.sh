1cb1ef01ec3330abac8a7fecbacd13e09e1f276c
arieycliu <ariel.yc.liu@gmail.com>
2025-12-03 11:39:30 -0500

    remove seed call from backend

--- backend/start.sh
+++ backend/start.sh
@@ -6,10 +6,11 @@ npx prisma migrate dev
 echo "generate schema"
 npx prisma generate
 
+echo "try importing generated schema"
 node prisma/client.js
 
 echo "create superuser"
 node prisma/createsu.js catis160 cats@mail.utoronto.ca Catsarec00!
 
-echo "seed db"
-node prisma/seed.js 
+# echo "seed db"
+# node prisma/seed.js 

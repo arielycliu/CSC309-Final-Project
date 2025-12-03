468afc736a8279fdd4009c31dff53f1357e7f93a
arieycliu <ariel.yc.liu@gmail.com>
2025-12-03 13:42:33 -0500

    reimplement seed commands

--- backend/start.sh
+++ backend/start.sh
@@ -3,14 +3,17 @@
 echo "drop and reset database"
 npx prisma migrate reset --force
 
-echo "generate schema"
+echo "create migration and apply it"
+npx prisma migrate dev --name init
+
+echo "generate schema (optional since we ran dev)"
 npx prisma generate
 
 echo "try importing generated schema"
 node prisma/client.js
 
-# echo "create superuser"
-# node prisma/createsu.js catis160 cats@mail.utoronto.ca Catsarec00!
+echo "create superuser"
+node prisma/createsu.js catis160 cats@mail.utoronto.ca Catsarec00!
 
-# echo "seed db"
-# node prisma/seed.js 
+echo "seed db"
+node prisma/seed.js 
